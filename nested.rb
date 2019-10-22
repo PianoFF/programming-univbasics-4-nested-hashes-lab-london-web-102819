0a79d5ff16913b9ede8ce42b2395ee00c946ba84
Fangfei Chen <fangfei1star@gmail.com>
2019-10-22 20:24:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,10 +105,8 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     programmer_hash[:alan_kay][:alans_new_info] = "GUI"
-		 programmer_hash[:alan_kay][:known_for].delete
-		 p programmer_hash
-     
+     programmer_hash[:alan_kay][:known_for].replace({:known_for => "Object Orientation"})
+		 
      
 end
 
